d1c4986d410c6394c6eba6c80b23e47413fe6d7e
Ryan Christian <ryanchristian4427@gmail.com>
2020-09-10 21:46:08 -0500

    refactor: Removing testing stuff for now

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    preset: 'jest-preset-preact',
-    automock: false,
-    collectCoverageFrom: ['src/Calendar/*.{ts,tsx}'],
-    setupFiles: ['<rootDir>/test/setup.ts'],
-    snapshotSerializers: ['enzyme-to-json/serializer'],
-};

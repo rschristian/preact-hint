33de9d4abf94fc2092279dcc7e77768ad1f3d35f
Ryan Christian <ryanchristian4427@gmail.com>
2020-09-11 15:28:38 -0500

    refactor: Updating demo

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,73 @@
-import { h, VNode } from 'preact';
+import { Fragment, h, VNode } from 'preact';
 
 import PreactHint from './Hint';
+import './style.css';
+
+type Contribution = {
+    date: string;
+    count: number;
+    intensity: number;
+};
+
+import data from './data.json';
 
 export default function App(): VNode {
+    const graphData: { total: number; contributions: Contribution[][] } = data;
+    const contributionColorArray = ['#ededed', '#62A197', '#428892', '#296887', '#274969'];
+
+    function createRects(): VNode[] {
+        return graphData.contributions
+            .map((week) =>
+                week.map((day, y) => (
+                    <rect
+                        key={day.date}
+                        x="0"
+                        y={(12 + 2) * y}
+                        width={12}
+                        height={12}
+                        fill={contributionColorArray[day.intensity]}
+                        data-preact-hint={[day.count, day.date]}
+                    />
+                )),
+            )
+            .map((week, x) => (
+                <g key={x} transform={`translate(${(12 + 2) * x}, 0)`}>
+                    {week}
+                </g>
+            ));
+    }
+
     return (
-        <div class="preview">
-            <PreactHint>
-                <button>Hello World</button>
-                <button data-preact-hint="Hello World">Hello Ryan</button>
-                <button>Hello World</button>
-                <button data-preact-hint="Hello World">Hello Ryan</button>
-                <button>Hello World</button>
-                <button data-preact-hint="Hello World">Hello Ryan</button>
-            </PreactHint>
-        </div>
+        <Fragment>
+            <section class="hero">
+                <div class="hero-body">
+                    <div class="container">
+                        <h1 class="is-size-2">Preact Hint</h1>
+                    </div>
+                </div>
+            </section>
+            <div class="container">
+                <div class="tile box preview">
+                    <PreactHint
+                        template={(content: string): VNode => {
+                            const stringPieces = content.split(',');
+                            return (
+                                <Fragment>
+                                    <strong>{stringPieces[0]} Contributions</strong> on {stringPieces[1]}
+                                </Fragment>
+                            );
+                        }}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            viewBox={`0 0 ${53 * (12 + 2) - 2} ${(12 + 2) * 7 - 2}`}
+                        >
+                            {createRects()}
+                        </svg>
+                    </PreactHint>
+                </div>
+            </div>
+        </Fragment>
     );
 }

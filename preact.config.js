39d18336a51b688eefd0dc8e2e1568564798d0f5
Ryan Christian <ryanchristian4427@gmail.com>
2020-09-13 22:38:28 -0500

    perf: Adding purgecss to reduce Bulma down

---
+++ preact.config.js
@@ -0,0 +1,13 @@
+import purgeCss from '@fullhuman/postcss-purgecss';
+
+export default {
+    webpack(config, env, helpers) {
+        const purgecss = purgeCss({
+            content: ['./src/**/*.tsx', './src/**/*.ts', './src/**/*.css'],
+            whitelist: ['html', 'body'],
+        });
+
+        const postCss = helpers.getLoadersByName(config, 'postcss-loader')[0];
+        if (env.production) postCss.loader.options.plugins.push(purgecss);
+    },
+};

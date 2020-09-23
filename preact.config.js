439fb73e8dd4cab25449bfc6bf16fde2dcd35265
Ryan Christian <ryanchristian4427@gmail.com>
2020-09-23 02:29:17 -0500

    fix: Resolving sideEffect bug until PR for CLI gets merged

--- preact.config.js
+++ preact.config.js
@@ -2,12 +2,18 @@ import purgeCss from '@fullhuman/postcss-purgecss';
 
 export default {
     webpack(config, env, helpers) {
+        const nonModuleStyleLoader = config.module.rules[5];
+        nonModuleStyleLoader.sideEffects = true;
+
         const purgecss = purgeCss({
             content: ['src/**/*.tsx', 'src/**/*.ts'],
             whitelist: ['html', 'body'],
         });
 
         const postCss = helpers.getLoadersByName(config, 'postcss-loader')[0];
-        if (env.production) postCss.loader.options.plugins.push(purgecss);
+        if (env.production) {
+            // config.output.publicPath = 'https://rschristian.github.io/preact-hint';
+            postCss.loader.options.plugins.push(purgecss);
+        }
     },
 };

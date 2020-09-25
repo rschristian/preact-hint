001b573f0d36d59feff15b7596c9312cc4c0962c
Ryan Christian <ryanchristian4427@gmail.com>
2020-09-24 19:52:51 -0500

    refactor: Adding postcss-nested to improve CSS experience

--- preact.config.js
+++ preact.config.js
@@ -1,4 +1,5 @@
 import purgeCss from '@fullhuman/postcss-purgecss';
+import nestedCss from 'postcss-nested';
 
 export default {
     webpack(config, env, helpers) {
@@ -11,9 +12,11 @@ export default {
         });
 
         const postCss = helpers.getLoadersByName(config, 'postcss-loader')[0];
+        postCss.loader.options.plugins.push(nestedCss);
         if (env.production) {
             config.output.publicPath = 'https://rschristian.github.io/preact-hint';
             postCss.loader.options.plugins.push(purgecss);
         }
+        console.log(postCss.loader.options.plugins);
     },
 };

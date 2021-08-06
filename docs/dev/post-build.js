34b5867680e0c99f2663080d5d148da21f3b53df
Ryan Christian <ryanchristian4427@gmail.com>
2021-08-06 17:49:55 -0500

    fix: Correcting docs build scripts

--- docs/dev/post-build.js
+++ docs/dev/post-build.js
@@ -4,5 +4,5 @@ import { promises as fs } from 'fs';
     let module = await fs.readFile('docs/dev/preact-hint.js', { encoding: 'utf-8' });
     module = module.replace(/(?<=from")(preact)/g, 'https://cdn.skypack.dev/preact');
     await fs.writeFile('docs/preact-hint.js', module);
-    await fs.copyFile('docs/dev/style.css', 'docs/preact-hint.css');
+    await fs.copyFile('docs/dev/preact-hint.css', 'docs/preact-hint.css');
 })();
